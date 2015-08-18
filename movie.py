bdbddb164a82a3cf9b2673006caae4274948a420
jmclinn <jonathanmclinn@gmail.com>
2015-08-18 18:55:27 -0500

    movie.py compatible with sphere

--- movie.py
+++ movie.py
@@ -10,14 +10,19 @@ import os,time
 file_is_variable = False
 
 ## If file_is_variable = True
-path_to_files = '/scratch/haluie/Ocean/POPdata/'
-file_part1 = 'h.14c.'
+## --------------------------
+## make sure to leave trailing slash '/' on 'path_to_files'
+path_to_files = '/path/to/files/'
+## For series of files with similar prefixes (file_part1) and filetypes (file_part2)
+file_part1 = 'pre.fixes.'
 file_part2 = '.nc'
-dates_list_text_file = '/scratch/haluie/Ocean/POPdata/dates_short.txt'
+## location of file listing (with each entry on a new line) the variable part of the filename
+dates_list_text_file = '/path/to/file/variable_list.txt'
 
 ## If file_is_variable = False
-file = '/scratch/haluie/Ocean/POPdata/h.14c.19980302.nc'
-
+## ---------------------------
+#file = '/path/to/single/file.nc'
+file = '/Users/Jon/Documents/other_projects/Aluie/visuals/1-12/mapdraw/sgs.nc'
 
 ## Variables (USER INPUT)
 ## ======================
@@ -40,6 +45,26 @@ title = '_empty_'
 crop = '_empty_'
 lines = '_empty_'
 
+
+## Sphere (for mapping onto Earth's spherical representation)
+## ----------------------------------------------------------
+
+## For use of 'sphere' set to True. If not leave False.
+sphere_mapping = False
+
+## Number of images (must match other variable list lengths from above)
+sphere_frames = 3
+
+## Start and stop points of sphere rotation (leave start/stop the same for no rotation in lat/lon)
+sphere_lon_start = -10
+sphere_lon_stop = 10
+
+sphere_lat_start = -10
+sphere_lat_stop = 10
+
+## 'zoom' argument described in README file (leave False if zoom = 1)
+zoom = 1.5
+
 ## Primary Variable (USER INPUT)
 ## =============================
 ## choose from the variables above
@@ -54,14 +79,14 @@ primary_variable = file
 ## provide folder location (without filename(s))
 ## ---------------------------------------------
 
-save = '/scratch/haluie/Ocean/AnalOutput/movie/'
+save = '/Users/Jon/Desktop/'
 
 ## Image Filename Prefix (USER INPUT)
 ## ==================================
 ## prefix for output filenames before auto-incremented counter
 ## -----------------------------------------------------------
 
-file_prefix = 'SGSmov_'
+file_prefix = 'img_'
 
 ## Image Counter Start (USER INPUT)
 ## ================================
@@ -98,6 +123,16 @@ if file_is_variable:
       file1.append(str(path_to_files) + str(file_part1) + str(line) + str(file_part2))
    file = file1
    primary_variable = file
+
+## Parsing of 'sphere' rotation inputs (Do Not Alter)
+## ==================================================
+if sphere_mapping:
+   lon_step = ( sphere_lon_stop - sphere_lon_start ) / ( sphere_frames - 1 )
+   lat_step = ( sphere_lat_stop - sphere_lat_start ) / ( sphere_frames - 1 )
+   sphere = []
+   for i in range(sphere_frames):
+      sphere.append(str(sphere_lon_start + lon_step * i)+','+str(sphere_lat_start + lat_step * i))
+   primary_variable = sphere
 
 
 ## Defining & Executing Command Expression (Do Not Alter)
@@ -217,8 +252,21 @@ for i in range(loop_len):
    if clr_min_maxi != '_empty_':
       clr_min_maxx = ' clr_min_max ' + str(clr_min_maxi)
       command = command + clr_min_maxx
+      
+   if sphere_mapping:
+      spherei = sphere[i]
+      spherex = ' sphere ' + str(spherei)
+      command = command + spherex
+      
+   if type(zoom) is list:
+      zoomi = zoom[i]
+   elif zoom:
+      zoomi = zoom
+   if zoom:
+      zoomx = ' zoom ' + str(zoomi)
+      command = command + zoomx
    
    time0 = time.time()
    os.system('python map.py ' + command)
    if display == 'no':
-      print str(i) + ' - ' + str(time.time() - time0) + ' sec'
+      print str(i) + ' - ' + str(round((time.time() - time0),2)) + ' sec'
